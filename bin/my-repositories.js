0600980a2a1bb9165173979a3174f3425d6add32
WindomZ <git.windomz@gmail.com>
2017-08-10 00:05:26 +0800

    feat: add github api v4

---
+++ bin/my-repositories.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+/**
+ * Created by WindomZ on 2017/8/1.
+ */
+'use strict';
+
+const User = require('../lib/github/user');
+
+let user = new User();
+user.repositories().then(r => {
+  console.log('totalCount: %f', r.totalCount)
+  console.log('totalDiskUsage: %f', r.totalDiskUsage)
+  console.log('nodes.length: %f', r.nodes.length)
+  console.log('nodes[0]: %j', r.nodes[0])
+}).catch(e => console.error(e));
