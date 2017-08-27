53b12cafd039db34e66e9c7f53b144f4128d3c4e
WindomZ <git.windomz@gmail.com>
2017-08-27 12:08:52 +0800

    feat: add config

--- bin/my-repositories.js
+++ bin/my-repositories.js
@@ -7,9 +7,12 @@
 const User = require('../lib/github/user');
 
 let user = new User();
-user.repositories().then(r => {
-  console.log('totalCount: %f', r.totalCount)
-  console.log('totalDiskUsage: %f', r.totalDiskUsage)
-  console.log('nodes.length: %f', r.nodes.length)
-  console.log('nodes[0]: %j', r.nodes[0])
-}).catch(e => console.error(e));
+user
+  .repositories()
+  .then(r => {
+    console.log('totalCount: %f', r.totalCount);
+    console.log('totalDiskUsage: %f', r.totalDiskUsage);
+    console.log('nodes.length: %f', r.nodes.length);
+    console.log('nodes[0]: %j', r.nodes[0]);
+  })
+  .catch(e => console.error(e));
---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by WindomZ on 17-8-27.
+ */
+'use strict';
+
+module.exports = {
+  github_token: 'MDE4ODc2YWYwYmRhOTQ5OGM5ZmEwYzdjYzAxMmMxNTlmN2M2MWQ3Mg==',
+};
